80e4521d3436f03f46746523aad063d2f01d0054
Antonio Daivan <contato.daivan@gmail.com>
2022-01-31 19:56:29 -0300

    Implementação do Intl.DateTimeFormat

--- script.js
+++ script.js
@@ -4,25 +4,21 @@ const screenDesbloqueio = document.getElementById("screen-desbloqueio")
 const body = document.getElementsByTagName("body")[0]
 const screenDate = document.getElementById("screenDate")
 const screenTime = document.getElementById("screenTime")
-const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-const weekDay = ['Domingo','Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira','Sábado']
+
+const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+}
 
 // Gera todo o relógio.
 function gerateClock() {
 
-    // Adiciona o '0' na frente caso o numero seja menor 10.  
-    function addZero(numero){
-        if(numero < 10){
-            return "0" + numero
-        }else{
-            return numero
-        }        
-    }
-
     let ndata = new Date() // Pega a data atual.
-    let time = ndata.toLocaleTimeString() // Retorna uma string com a hora local.
-    let data = ((weekDay[ndata.getDay()]) + ", " + (addZero((ndata.getDate()).toString()) + " " + "de " + (months[ndata.getMonth()]) + " de " + (ndata.getFullYear()) + ".")) // Retorna a data completa.
-
+    let time = ndata.toLocaleTimeString("pt-br") // Retorna uma string com a hora local.
+    let data = new Intl.DateTimeFormat("pt-br", {
+        dateStyle: "full"
+    }).format(ndata) + "." // Retorna a data
 
     screenDate.textContent = data // Add a data na tela
     screenTime.textContent = time // Add a hora na tela
