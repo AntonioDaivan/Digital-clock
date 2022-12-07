4bf4577c7bd0ad2edf8365bbd1cb217b1e01930b
Antonio Daivan <contato.daivan@gmail.com>
2022-12-07 16:31:26 -0300

    Remoção tela de bloqueio

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Tela de bloqueio.
+# Relógio digital.
 
-## Tela de bloqueio inspirada na tela do Ubuntu 20.04.
+## Relógio digital inspirado na tela do Ubuntu 20.04.
 
 ### <a href="https://antoniodaivan.github.io/Digital-clock/">Acesse aqui</a>.
--- script.js
+++ script.js
@@ -5,12 +5,6 @@ const body = document.getElementsByTagName("body")[0]
 const screenDate = document.getElementById("screenDate")
 const screenTime = document.getElementById("screenTime")
 
-const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-}
-
 // Gera todo o relógio.
 function gerateClock() {
 
@@ -25,16 +19,3 @@ function gerateClock() {
 }
 
 const displayClock = setInterval(gerateClock, 1000) // Chama a função gerateClick a cada 1 segundo
-
-function desbloqueioScreen (e){
-    if(e.which == "13"){
-        screen.classList.toggle("desbloqueio")
-        screenDesbloqueio.classList.toggle("reset")
-        screenDesbloqueio.classList.toggle("desbloqueio")
-
-        document.getElementById("password").value = ""
-    }
-}
-
-// body.addEventListener("click", desbloqueioScreen)
-body.addEventListener("keypress", desbloqueioScreen)
