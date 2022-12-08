bb4fc5af6c8ab8af324cac2ab45a5b47e0f38c44
Antonio Daivan <contato.daivan@gmail.com>
2022-12-08 08:41:27 -0300

    dark mode

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Variáveis 
 const screen = document.getElementById("screen")
 const screenDesbloqueio = document.getElementById("screen-desbloqueio")
-const body = document.getElementsByTagName("body")[0]
+const body = document.querySelector('body')
 const screenDate = document.getElementById("screenDate")
 const screenTime = document.getElementById("screenTime")
+const themeIcon = document.querySelector('.bx')
 
 // Gera todo o relógio.
 function gerateClock() {
@@ -19,3 +20,12 @@ function gerateClock() {
 }
 
 const displayClock = setInterval(gerateClock, 1000) // Chama a função gerateClick a cada 1 segundo
+
+// Alterar para tema claro e escuro
+const changeTheme = _ => {
+    body.classList.toggle('dark')
+    themeIcon.classList.toggle('bxs-moon')
+    themeIcon.classList.toggle('bxs-sun')
+}
+
+themeIcon.addEventListener('click', changeTheme)
